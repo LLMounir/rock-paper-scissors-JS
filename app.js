b3d1e3d3f6117013b0fc48b00da8d9fd1c1d32a6
ml20 <mounirlalami@hotmail.com>
2020-06-27 01:42:34 +0200

    Aniamtions

--- app.js
+++ app.js
@@ -4,68 +4,73 @@ const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board")
 const result_div = document.querySelector(".result > p")
-const rock_div = document.getElementById("rock")
-const paper_div = document.getElementById("paper")
-const scissors_div = document.getElementById("scissors")
+const rock_div = document.getElementById("r")
+const paper_div = document.getElementById("p")
+const scissors_div = document.getElementById("s")
 
 function getComputerChoice(){
     const choices = ['r','p','s'];
     return choices[Math.floor(Math.random()*3)];
 }
 
-function win(){
+function win(userChoice){
     userScore++;
     userScore_span.innerHTML = userScore;
+    document.getElementById(userChoice).classList.add('green-glow');
+    setTimeout(function() { document.getElementById(userChoice).classList.remove('green-glow') }, 400);
 }
 
-function lose() {
+function lose(userChoice) {
     computerScore++;
     computerScore_span.innerHTML = computerScore;
+    document.getElementById(userChoice).classList.add('red-glow');
+    setTimeout(function () { document.getElementById(userChoice).classList.remove('red-glow') }, 400);
 
 }
-function draw() {
-
+function draw(userChoice) {
+    document.getElementById(userChoice).classList.add('gray-glow');
+    setTimeout(function () { document.getElementById(userChoice).classList.remove('gray-glow') }, 400);
 }
 
 function game(userChoice){
     const computerChoice = getComputerChoice();
     switch (userChoice + computerChoice){
         case "rr":
-            draw();
+            draw(userChoice);
             result_div.innerHTML = "Pierre contre Pierre. Égalité"
             break;
         case "ss":
-            draw();
+            draw(userChoice);
             result_div.innerHTML = "Ciseaux contre Ciseaux. Égalité"
             break;
         case "pp":
-            draw();
+            draw(userChoice);
             result_div.innerHTML = "Papier contre Papier. Égalité"
             break;
 
         case "rp":
-            lose();
+            lose(userChoice);
             result_div.innerHTML = "Le papier couvre la pierre. Vous perdez!"
             break;
         case "sr":
-            lose();
+            lose(userChoice);
             result_div.innerHTML = "La pierre casse les ciseaux. Vous perdez!"
             break;
         case "ps":
-            lose();
+            lose(userChoice);
             result_div.innerHTML = "Les ciseaux coupent le papier. Vous perdez!"
             break;
             
         case "rs":
-            win();
+            win(userChoice);
             result_div.innerHTML = "La pierre casse les ciseaux. Vous gagnez!"
             break;
         case "pr":
-            win();
+            win(userChoice);
             result_div.innerHTML = "Le papier couvre la pierre. Vous gagnez!"
             break;
         case "sp":
-            win();
+            win(userChoice);
             result_div.innerHTML = "Les ciseaux coupent le papier. Vous gagnez!"
             break;
     }
